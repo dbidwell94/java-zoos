107e206aaa99172db4c0b8a16ac08f0ecda05f1e
Devin Bidwell <dbidwell@intuituscyber.com>
2020-09-10 13:47:23 -0700

    mvp finished

--- src/main/java/com/lambdaschool/javazoos/services/ZooServicesImpl.java
+++ src/main/java/com/lambdaschool/javazoos/services/ZooServicesImpl.java
@@ -5,6 +5,7 @@ import com.lambdaschool.javazoos.repositories.ZoosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,5 +20,12 @@ public class ZooServicesImpl implements ZooServices {
         List<Zoo> zoos = new ArrayList<>();
         zoosRepository.findAll().iterator().forEachRemaining(zoos::add);
         return zoos;
+    }
+
+    @Override
+    public Zoo getZooById(long id) {
+        Zoo z = zoosRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Zoo with id %d not found", id)));
+        return z;
     }
 }
